940f4cb8d22ecfe077121f5f63b1861035fc30b7
ALEX LIM <alim@ccs.neu.edu>
2020-02-03 22:46:01 -0500

    Create slide document

---
+++ studio/schemas/documents/slide.js
@@ -0,0 +1,91 @@
+import {format} from 'date-fns'
+
+export default {
+  name: 'slide',
+  type: 'document',
+  title: 'Slide',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+      description: 'Titles should be catchy, descriptive, and not too long'
+    },
+    {
+      name: 'publishedAt',
+      type: 'datetime',
+      title: 'Published at',
+      description: 'This can be used to schedule post for publishing'
+    },
+    {
+      name: 'mainImage',
+      type: 'mainImage',
+      title: 'Main image'
+    },
+    {
+      name: 'categories',
+      type: 'array',
+      title: 'Categories',
+      of: [
+        {
+          type: 'reference',
+          to: {
+            type: 'category'
+          }
+        }
+      ]
+    },
+    {
+      name: 'body',
+      type: 'bodyPortableText',
+      title: 'Body'
+    }
+  ],
+  orderings: [
+    {
+      name: 'publishingDateAsc',
+      title: 'Publishing date new–>old',
+      by: [
+        {
+          field: 'publishedAt',
+          direction: 'asc'
+        },
+        {
+          field: 'title',
+          direction: 'asc'
+        }
+      ]
+    },
+    {
+      name: 'publishingDateDesc',
+      title: 'Publishing date old->new',
+      by: [
+        {
+          field: 'publishedAt',
+          direction: 'desc'
+        },
+        {
+          field: 'title',
+          direction: 'asc'
+        }
+      ]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      publishedAt: 'publishedAt',
+      slug: 'slug',
+      media: 'mainImage'
+    },
+    prepare ({title = 'No title', publishedAt, slug = {}, media}) {
+      const dateSegment = format(publishedAt, 'YYYY/MM')
+      const path = `/${dateSegment}/${slug.current}/`
+      return {
+        title,
+        media,
+        subtitle: publishedAt ? path : 'Missing publishing date'
+      }
+    }
+  }
+}
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,6 +8,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
+import slide from './documents/slide'
 import siteSettings from './documents/siteSettings'
 
 // Object types
@@ -28,6 +29,7 @@ export default createSchema({
     // in the studio.
     siteSettings,
     post,
+    slide,
     category,
     author,
     mainImage,
