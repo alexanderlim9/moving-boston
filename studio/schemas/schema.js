2b44ce6233d83c0a8a28886d4991aa9be3bbb479
ALEX LIM <alim@ccs.neu.edu>
2020-02-04 11:13:37 -0500

    Create hompage document

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,6 +8,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
+import homepage from './documents/homepage'
 import siteSettings from './documents/siteSettings'
 
 // Object types
@@ -27,6 +28,7 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     siteSettings,
+    homepage,
     post,
     category,
     author,
