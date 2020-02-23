40b86714d2e7fec53b379eda3bb349313b7d0924
ALEX LIM <alim@ccs.neu.edu>
2020-02-23 17:51:08 -0500

    Create documents for each question

---
+++ studio/schemas/documents/question2.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'question2',
+    type: 'document',
+    title: 'Question 2',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'slug',
+        type: 'slug',
+        title: 'Slug'
+      },
+      {
+        name: 'description',
+        type: 'text',
+        title: 'Description'
+      }
+    ]
+  }
+  
