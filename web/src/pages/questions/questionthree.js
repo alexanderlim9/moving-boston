91b2c7ab61d46c1e203a5417beb18022cfbf6f02
ALEX LIM <alim@ccs.neu.edu>
2020-03-22 10:11:12 -0400

    Start to set up ant design slider

--- web/src/pages/questions/questionthree.js
+++ web/src/pages/questions/questionthree.js
@@ -6,7 +6,9 @@ import Layout from "../../containers/layout";
 
 import "../../styles/questions/question-three-styles.css";
 import "../../styles/questions/question-general-styles.css";
-// import Frequency from "../../components/questioncomponents/frequency/frequency";
+// import Slider from "../../components/questioncomponents/slider/slider";
+import { Slider, Switch } from 'antd';
+
 
 export const query = graphql`
   query QuestionThreeQuery {
@@ -49,33 +51,6 @@ const QuestionThree = props => {
   }
 
   const [isOnIntro, setIsOnIntro] = useState(true);
-  const [answerOptions, setAnswerOptions] = useState([
-    "Never",
-    "Sometimes",
-    "Most of the time",
-    "Always"
-  ]);
-  const [options, setOptions] = useState({
-    Train: { image: null, selectedValue: null },
-    Bus: { image: null, selectedValue: null },
-    T: { image: null, selectedValue: null },
-    Car: { image: null, selectedValue: null },
-    Bike: { image: null, selectedValue: null },
-    Walk: { image: null, selectedValue: null },
-    Ferry: { image: null, selectedValue: null },
-    Rideshare: { image: null, selectedValue: null },
-    Scooter: { image: null, selectedValue: null }
-  });
-
-  const setSpecificOption = (opt, value) => {
-    setOptions({
-      ...options,
-      [opt]: {
-        image: opt.image,
-        selectedValue: value
-      }
-    });
-  };
 
   return (
     <Layout>
@@ -91,11 +66,9 @@ const QuestionThree = props => {
           <div className="qintro__description">{qthreeData.introNextTitle}</div>
 
           <div className="qintro__cta" onClick={() => setIsOnIntro(true)}>debug back</div>
-          {/* <Frequency
-            setSpecificCallback={(opt, value) => setSpecificOption(opt, value)}
-            options={options}
-            answerOptions={answerOptions}
-          /> */}
+          <div>
+            <Slider defaultValue={30}/>
+          </div>
         </div>
       </div>
     </Layout>
