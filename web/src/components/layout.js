cd5e700cc088d646a1ef0c921548317894a01e39
melvin-chen <chen.melv@husky.neu.edu>
2020-04-16 14:50:33 -0400

    misc styling changes

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import Header from './header'
+import React, { useEffect } from "react";
+import Header from "./header";
 
-import '../styles/layout.css'
-import styles from './layout.module.css'
-import { Menu } from './menu/menu';
+import "../styles/layout.css";
+import styles from "./layout.module.css";
+import { Menu } from "./menu/menu";
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
-  <>
-    {/* <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} /> */}
-    <Menu/>
-    <div className={styles.content}>{children}</div>
-    {/* <footer className={styles.footer}>
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+  return (
+    <>
+      {/* <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} /> */}
+      <Menu />
+      <div className={styles.content}>{children}</div>
+      {/* <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
         <div className={styles.siteInfo}>
           &copy; {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a>{' '}
@@ -20,7 +24,8 @@ const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
         </div>
       </div>
     </footer> */}
-  </>
-)
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
