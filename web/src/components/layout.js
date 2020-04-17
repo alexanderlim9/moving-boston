e015a072e8221ec160d685dcae01180a04f19183
ALEX LIM <alim@ccs.neu.edu>
2020-04-17 16:42:18 -0400

    Create footer

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,25 +1,77 @@
 import React from 'react'
 import Header from './header'
-
+import { Link } from 'gatsby'
 import '../styles/layout.css'
 import styles from './layout.module.css'
 import { Menu } from './menu/menu';
+import { propTypes } from '@sanity/block-content-to-react';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  LinkedinShareButton,
+  EmailIcon,
+  EmailShareButton
+} from "react-share";
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
+const shareUrl = "https://moving-boston-web.netlify.com/";
+const iconSize = 28;
+
+const Layout = ({children, displayFooter, onHideNav, onShowNav, showNav, siteTitle}) => (
   <>
     {/* <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} /> */}
     <Menu/>
     <div className={styles.content}>{children}</div>
-    {/* <footer className={styles.footer}>
-      <div className={styles.footerWrapper}>
-        <div className={styles.siteInfo}>
-          &copy; {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a>{' '}
-          &amp;
-          {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+    <footer className={displayFooter ? styles.visible : styles.hidden}>
+      <div className={styles.footerRow1}>
+        <span>Moving Boston 2030</span>
+        <div className={styles.navLinks}>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/questions/questionone"}>Survey</Link>
+          <Link to={"/about"}>About</Link>
         </div>
       </div>
-    </footer> */}
+      <div className={styles.footerRow2}>
+        <span>&copy; {new Date().getFullYear()}</span>
+        <div className={styles.socialLinks}>
+          <p>Share this experience </p>
+          <EmailShareButton url={shareUrl}>
+            <EmailIcon
+              size={iconSize}
+              borderRadius={5}
+              iconFillColor={"#2E2E2E"}
+              bgStyle={{ fill: "white" }}
+            />
+          </EmailShareButton>
+          <FacebookShareButton url={shareUrl}>
+            <FacebookIcon
+              size={iconSize}
+              borderRadius={5}
+              iconFillColor={"#2E2E2E"}
+              bgStyle={{ fill: "white" }}
+            />
+          </FacebookShareButton>
+          <TwitterShareButton url={shareUrl}>
+            <TwitterIcon
+              size={iconSize}
+              borderRadius={5}
+              iconFillColor={"#2E2E2E"}
+              bgStyle={{ fill: "white" }}
+            />
+          </TwitterShareButton>
+          <LinkedinShareButton url={shareUrl}>
+            <LinkedinIcon
+              size={iconSize}
+              borderRadius={5}
+              iconFillColor={"#2E2E2E"}
+              bgStyle={{ fill: "white" }}
+            />
+          </LinkedinShareButton>
+        </div>
+      </div>
+    </footer>
   </>
 )
 
