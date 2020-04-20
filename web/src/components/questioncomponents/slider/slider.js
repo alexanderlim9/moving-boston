bbbf2c5a357ec0fb7520f3dd45e64431937ec064
ALEX LIM <alim@ccs.neu.edu>
2020-04-19 22:11:15 -0400

    Disable slider upon reset

--- web/src/components/questioncomponents/slider/slider.js
+++ web/src/components/questioncomponents/slider/slider.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Icon from "../../icon";
-import Draggable from 'react-draggable'; // The default
+import Draggable from 'react-draggable';
 import "./slider-styles.css";
 
 const Slider = props => {
@@ -25,6 +25,7 @@ const Slider = props => {
         axis="x"
         bounds="parent"
         defaultPosition={{x: startX, y: 0}}
+        disabled={props.zero}
         >
           <div className="slider-span">
             <div id="slider-circle" style={ props.zero ? {transform: `translate(-${getXPos(props.sliderNumber)-80}px, 0px)`, transition: 'transform 2s ease-in-out'} : null}/>
